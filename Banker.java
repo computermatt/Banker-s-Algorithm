501ecbf98afdfc18569a2b09c83cec1f14f70ddb
David Wilson <dmw9070@rit.edu>
2014-10-10 16:44:11 -0400

    Banker.request(); Banker.safe() stub
    renamed current->allocated, added released units to nResources

--- Banker.java
+++ Banker.java
@@ -13,7 +13,7 @@ public class Banker {
 
 	private int nResources;
 
-	private Map<Thread, Integer> current = Collections.synchronizedMap( new HashMap<Thread, Integer>());
+	private Map<Thread, Integer> allocated = Collections.synchronizedMap( new HashMap<Thread, Integer>());
 	private Map<Thread, Integer> remaining = Collections.synchronizedMap( new HashMap<Thread, Integer>());
 	
 	public Banker( int nUnits ) {
@@ -24,41 +24,57 @@ public class Banker {
 		Thread currentThread = Thread.currentThread();
 		if( nUnits <= 0 || nUnits > nResources || hasClaim( currentThread ) )
 			System.exit(1);
-		current.put( currentThread, 0 );
+		allocated.put( currentThread, 0 );
 		remaining.put( currentThread, nUnits );
 		System.out.println( String.format( CLAIM, currentThread.getName(), nUnits ) );
 	}
 	
 	public synchronized boolean request( int nUnits ) {
-		//TODO
-		// print message: Thread /name/ requests /nUnits/ units.
-		// If allocating the resources results in a safe state,
-		// -print message: Thread /name/ has /nUnits/ units allocated.
-		// update banker's state and return to the caller.
-		// Otherwise enter a loop that:
-		// -prints message: Thread /name/ waits.
-		// -waits for notification of a change
-		// -when reawakened, prints message: Thread /name/ awakened.
-		// -if allocating resource results in safe state, prints message:
-		// --Thread /name/ has /nUnits/ units allocated.
-		// --updates banker's state and returns
 		Thread currentT = Thread.currentThread();
-		if(nUnits <= 0 || !this.hasClaim(currentT) || this.current.get(currentT))
+		if(nUnits <= 0 || !this.hasClaim(currentT) || nUnits > this.remaining.get(currentT))
 			System.exit(1);
-		return false;
+		
+		System.out.println( String.format( REQUEST, currentT.getName(), nUnits ));
+
+		// check potential state
+		int[] allocatedArray = new int[allocated.size()];
+		int[] remainingArray = new int[remaining.size()];
+		fillArraysForPotentialState( allocatedArray, remainingArray, currentT, nUnits );
+		
+		while( !safe( nResources - nUnits, allocatedArray, remainingArray ) ) {
+			// wait
+			System.out.println( String.format( WAIT, currentT.getName() ) );
+			try {
+				wait();
+			} catch( InterruptedException e ) {
+				e.printStackTrace();
+			}
+			System.out.println( String.format( WAKE, currentT.getName() ) );
+			
+			// check again with new state
+			fillArraysForPotentialState( allocatedArray, remainingArray, currentT, nUnits );
+		}
+		
+		// allocate resources
+		System.out.println( String.format( ALLOCATE, currentT.getName(), nUnits ) );
+		allocated.put( currentT, allocated.get( currentT ) + nUnits );
+		remaining.put( currentT, remaining.get( currentT ) - nUnits );
+		nResources -= nUnits;
+		return true;
 	}
 	
 	public synchronized void release( int nUnits ) {
 		Thread currentT = Thread.currentThread();
-		if(nUnits <= 0 || !this.hasClaim(currentT) || nUnits > this.current.get(currentT))
+		if(nUnits <= 0 || !this.hasClaim(currentT) || nUnits > this.allocated.get(currentT))
 			System.exit(1);
 		System.out.println(String.format(RELEASE, currentT.getName(), nUnits));
-		this.current.put(currentT, this.current.get(currentT) - nUnits);
+		this.allocated.put(currentT, this.allocated.get(currentT) - nUnits);
+		nResources += nUnits;
 		notifyAll();
 	}
 	
 	public synchronized int allocated() {
-		return current.get( Thread.currentThread() );
+		return allocated.get( Thread.currentThread() );
 	}
 	
 	public synchronized int remaining() {
@@ -66,6 +82,23 @@ public class Banker {
 	}
 	
 	private boolean hasClaim( Thread t ) {
-		return current.containsKey( t ) && remaining.containsKey( t ) && current.get( t ) + remaining.get( t ) > 0;
+		return allocated.containsKey( t ) && remaining.containsKey( t ) && allocated.get( t ) + remaining.get( t ) > 0;
+	}
+	
+	private void fillArraysForPotentialState( int[] allocated, int[] remaining, Thread currentThread, int nUnits ) {
+		int i = 0;
+		for( Thread t : this.allocated.keySet() ) {
+			if( t.equals( currentThread ) ) {
+				allocated[i] = this.allocated.get( t ) + nUnits;
+				remaining[i++] = this.remaining.get( t ) - nUnits;
+			} else {
+				allocated[i] = this.allocated.get( t );
+				remaining[i++] = this.remaining.get( t );
+			}
+		}
+	}
+	
+	private boolean safe( int resources, int[] allocated, int[] remaining ) {
+		return false;
 	}
 }
